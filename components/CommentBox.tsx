0e5359ace0f27ca2f0ccba2f411f7553e140e245
Diego Sarmiento <dlsarmiento@gmail.com>
2022-06-25 18:51:15 -0400

    Added form to Component

--- components/CommentBox.tsx
+++ components/CommentBox.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const CommentBox = () => {
-    return(
-        <div>
-            <h3>Comment Box</h3>
-            <ul>
-                <li>Soon</li>
-            </ul>
-        </div>
-    )
+class CommentBox extends React.Component<{comment: string}, {comment: string}> {
+    
+    title = 'Add comment';
+    buttonText = 'Submit Comment';
+    state = { comment: ''};
+    
+    handleChange = (event: any) => {
+        this.setState({ comment: event.target.value});
+    }
+    
+    handleSubmit = (event: any) => {
+        event.preventDefault();
+        //this.props.saveComment(this.state.comment);
+        this.setState({comment: ''});
+    }
+    
+    render () {
+        return(
+            <div>
+                <div>
+                    <form onSubmit={this.handleSubmit} role="box">
+                        <h4>{this.title}</h4>
+                        <textarea aria-label="text-comment" onChange={this.handleChange} value={this.state.comment} />
+                        <div>
+                            <button>{this.buttonText}</button>
+                        </div>
+                    </form>
+                    {/* TODO: onClick={this.props.fetchComments */}
+                    <button aria-label="async-button" >Fetch Comments</button>
+                </div>
+            </div>
+        )
+    }
 }
 
- export default CommentBox;
+export default CommentBox;
